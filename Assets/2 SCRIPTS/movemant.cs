2eb7d1d7eed93d000373a253a9c55b2e1f220e43
Juseong <hypyp7957@naver.com>
2025-05-20 16:20:43 +0900

    ADD(Movment /r type

--- Assets/2 SCRIPTS/movemant.cs
+++ Assets/2 SCRIPTS/movemant.cs
@@ -12,22 +12,13 @@ public class movemant : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //his.transform.position = this.transform.position + Vector3.forward * moveSpeed;
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        }
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.S)) direction += Vector3.back;
+        if (Input.GetKey(KeyCode.A)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) direction += Vector3.right;
+
+        transform.position += direction.normalized * moveSpeed * Time.deltaTime;
     }
 }
